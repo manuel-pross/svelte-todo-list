5477915f063c4b95409d6b2041f30842afbf676b
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2021-01-31 18:07:39 +0100

    TASK: button style

--- src/components/DeleteItems.svelte
+++ src/components/DeleteItems.svelte
@@ -18,11 +18,27 @@
     .delete-wrapper {
         display: flex;
         justify-content: space-between;
+        padding: 0 15px;
+    }
+
+    .btn {
+        cursor: pointer;
+        box-sizing: unset;
+        margin: 0;
+        padding: 0.25rem 0.75rem;
+        background-color: white;
+        border-radius: 20px;
+        min-width: 150px;
+        border: 1px solid black;
+    }
+
+    .btn:hover {
+        background-color: #dddd;
     }
 </style>
 
 
 <div class="delete-wrapper">
-    <button on:click={handleClickDeleteCompleted}>Delete completed</button>
-    <button on:click={handleClickDeleteAll}>Delete all</button>
+    <button class="btn" on:click={handleClickDeleteCompleted}>Delete completed</button>
+    <button class="btn" on:click={handleClickDeleteAll}>Delete all</button>
 </div>
